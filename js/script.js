a9764617a26de5c8a6b3fc7a9f84a9c3db601b73
rwcalinin <rwcalinin@gmail.com>
2021-05-05 14:14:58 +0300

    film-list script

--- js/script.js
+++ js/script.js
@@ -19,8 +19,33 @@ const movieDB = {
         "Логан",
         "Лига справедливости",
         "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        "Гарри Поттер",
+        "Пятый элемент"
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} - ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
